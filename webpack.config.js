bf54aa743f1629904ee7477165fce00b0cc482d0
Tuuu <tutengfei1994@gmail.com>
2019-04-05 20:46:51 +0800

    webpack 初次运行: index.tsx -> index.js

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './lib/index.tsx',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'), // 防止 windows 与 mac 路径不一致
+    library: 'TUUI', // 库的名称
+    libraryTarget: 'umd', // 库的模块化定义 common js / amd / umd
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, // 以 ts/tsx 结尾的文件
+        loader: 'awesome-typescript-loader',
+      },
+    ],
+  },
+}
