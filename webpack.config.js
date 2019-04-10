8c4835964019d22cf631f0b9ec81c00c982f22ed
Tuuu <tutengfei1994@gmail.com>
2019-04-10 22:38:13 +0800

    添加 SCSS & SCSS 相关 loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,31 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
   entry: {
-    index: './lib/index.tsx',
+    index: "./lib/index.tsx"
   },
   output: {
-    path: path.resolve(__dirname, 'dist/lib'), // 防止 windows 与 mac 路径不一致
-    library: 'TUUI', // 库的名称
-    libraryTarget: 'umd', // 库的模块化定义 common js / amd / umd
+    path: path.resolve(__dirname, "dist/lib"), // 防止 windows 与 mac 路径不一致
+    library: "TUUI", // 库的名称
+    libraryTarget: "umd" // 库的模块化定义 common js / amd / umd
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/, // 以 ts/tsx 结尾的文件
-        loader: 'awesome-typescript-loader',
+        loader: "awesome-typescript-loader"
       },
       {
         test: /icons.+\.svg$/,
-        loader: 'svg-sprite-loader',
+        loader: "svg-sprite-loader"
       },
-    ],
-  },
-}
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
+};
