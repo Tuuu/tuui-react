4861b0930a97922ff751b8f2e446d1b481144173
Tuuu <tutengfei1994@gmail.com>
2019-04-10 23:02:28 +0800

    Icon 添加 onClick 事件 & 声明事件类型

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -3,12 +3,13 @@ import './importAllIcons'
 import './icon.scss'
 
 interface IconProps {
-  name: string;
+  name: string,
+  onClick: React.MouseEventHandler<SVGElement>
 }
 
 const Icon: React.FunctionComponent<IconProps> = props => {
   return (
-    <svg className="tui-icon">
+    <svg className="tui-icon" onClick={props.onClick}>
       <use xlinkHref={`#${props.name}`}/>
     </svg>
   )
