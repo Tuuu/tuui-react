8bba1e4a1e2893ac21982ae9efe3900dcd69f48f
Tuuu <tutengfei1994@gmail.com>
2019-05-08 00:02:32 +0800

    button 添加事件监听

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -4,10 +4,32 @@ import Dialog from './dialog';
 
 export default function() {
   const [x, setX] = useState(false);
+  const [y, setY] = useState(false);
   return (
     <div>
+      <h1>例子1：</h1>
       <button onClick={() => setX(!x)}>click</button>
-      <Dialog visible={x}>
+      <Dialog
+        visible={x}
+        buttons={[
+          <button onClick={() => setX(false)}>1</button>,
+          <button onClick={() => setX(false)}>2</button>,
+        ]}
+        onclose={() => setX(false)}
+      >
+        <div>h1</div>
+      </Dialog>
+      <h1>例子2：</h1>
+      <button onClick={() => setY(!y)}>click</button>
+      <Dialog
+        visible={y}
+        closeOnClickMask={true}
+        buttons={[
+          <button onClick={() => setY(false)}>1</button>,
+          <button onClick={() => setY(false)}>2</button>,
+        ]}
+        onclose={() => setY(false)}
+      >
         <div>h1</div>
       </Dialog>
     </div>
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,31 +1,47 @@
-import * as React from 'react';
+import React, { ReactElement } from 'react';
 import './dialog.scss';
 import { Icon } from '../index';
 import { scopeClassMaker } from '../helpers/classes';
 
 interface Props {
   visible: boolean;
+  buttons: Array<ReactElement>;
+  onclose: React.MouseEventHandler;
+  closeOnClickMask?: boolean;
 }
 
 const scopeClass = scopeClassMaker('tui-dialog');
 
 const Dialog: React.FunctionComponent<Props> = props => {
+  const onClickClose: React.MouseEventHandler = e => {
+    props.onclose(e);
+  };
+  const onClickMask: React.MouseEventHandler = e => {
+    if (props.closeOnClickMask) props.onclose(e);
+  };
   return props.visible ? (
     <>
-      <div className={scopeClass('mask')} />
+      <div className={scopeClass('mask')} onClick={onClickMask} />
       <div className={scopeClass()}>
-        <div className={scopeClass('close')}>
+        <div className={scopeClass('close')} onClick={onClickClose}>
           <Icon name="close" />
         </div>
         <header className={scopeClass('header')}>提示</header>
         <main className={scopeClass('main')}>{props.children}</main>
         <footer className={scopeClass('footer')}>
-          <button>取消</button>
-          <button>确定</button>
+          {props.buttons.map((button, index) =>
+            React.cloneElement(button, {
+              key: index,
+            }),
+          )}
         </footer>
       </div>
     </>
   ) : null;
+};
+
+Dialog.defaultProps = {
+  closeOnClickMask: false,
 };
 
 export default Dialog;
