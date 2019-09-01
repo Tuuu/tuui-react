99ded1b6bf631e6b4b7bbe37440771b47a9b7339
Tuuu <tutengfei1994@gmail.com>
2019-09-01 17:27:15 +0800

    美化 layout 例子

--- lib/layout/layout.example.tsx
+++ lib/layout/layout.example.tsx
@@ -5,40 +5,42 @@ import Content from './content';
 import Footer from './footer';
 import Aside from './aside';
 
+import './layout.example.scss';
+
 export default () => {
   return (
     <div>
       <h1>第一个例子</h1>
-      <Layout className="hi" style={{ height: 300 }}>
-        <Header>header</Header>
-        <Content>content</Content>
-        <Footer>footer</Footer>
+      <Layout className="wrapper">
+        <Header className="header">header</Header>
+        <Content className="content">content</Content>
+        <Footer className="footer">footer</Footer>
       </Layout>
       <h1>第二个例子</h1>
-      <Layout style={{ height: 300 }}>
-        <Header>header</Header>
+      <Layout className="wrapper">
+        <Header className="header">header</Header>
         <Layout>
-          <Aside>aside</Aside>
-          <Content>content</Content>
+          <Aside className="aside">aside</Aside>
+          <Content className="content">content</Content>
         </Layout>
-        <Footer>footer</Footer>
+        <Footer className="footer">footer</Footer>
       </Layout>
       <h1>第三个例子</h1>
-      <Layout style={{ height: 300 }}>
-        <Header>header</Header>
+      <Layout className="wrapper">
+        <Header className="header">header</Header>
         <Layout>
-          <Content>content</Content>
-          <Aside>aside</Aside>
+          <Content className="content">content</Content>
+          <Aside className="aside">aside</Aside>
         </Layout>
-        <Footer>footer</Footer>
+        <Footer className="footer">footer</Footer>
       </Layout>
       <h1>第四个例子</h1>
-      <Layout style={{ height: 300 }}>
-        <Aside>aside</Aside>
+      <Layout className="wrapper">
+        <Aside className="aside">aside</Aside>
         <Layout>
-          <Header>header</Header>
-          <Content>content</Content>
-          <Footer>footer</Footer>
+          <Header className="header">header</Header>
+          <Content className="content">content</Content>
+          <Footer className="footer">footer</Footer>
         </Layout>
       </Layout>
     </div>
