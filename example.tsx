df8667ec95e3a88df79557e28a45fae20f4a9e2c
Tuuu <tutengfei1994@gmail.com>
2020-03-16 20:04:04 +0800

    example 样式调整、修复依赖告警

--- example.tsx
+++ example.tsx
@@ -1,41 +1,44 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, NavLink } from 'react-router-dom';
 import IconExample from './lib/icon/icon.example';
 import DialogExample from './lib/dialog/dialog.example';
 import LayoutExample from './lib/layout/layout.example';
+import Layout, { Aside, Content, Header, Footer } from './lib/layout/layout';
+import './example.scss';
 
 ReactDom.render(
   <Router>
-    <div>
-      <header>
+    <Layout>
+      <Header className="site-header">
         <div className="logo">tui</div>
-      </header>
-    </div>
-    <div>
-      <aside>
-        <h1>组件</h1>
-        <ul>
-          <li>
-            <Link to="/icon">Icon</Link>
-          </li>
-          <li>
-            <Link to="/button">Button</Link>
-          </li>
-          <li>
-            <Link to="/dialog">Dialog</Link>
-          </li>
-          <li>
-            <Link to="/layout">布局</Link>
-          </li>
-        </ul>
-      </aside>
-      <main>
-        <Route path="/icon" component={IconExample} />
-        <Route path="/dialog" component={DialogExample} />
-        <Route path="/layout" component={LayoutExample} />
-      </main>
-    </div>
+      </Header>
+      <Layout>
+        <Aside className="site-aside">
+          <h1>组件</h1>
+          <ul>
+            <li>
+              <NavLink to="/icon">Icon</NavLink>
+            </li>
+            <li>
+              <NavLink to="/button">Button</NavLink>
+            </li>
+            <li>
+              <NavLink to="/dialog">Dialog</NavLink>
+            </li>
+            <li>
+              <NavLink to="/layout">布局</NavLink>
+            </li>
+          </ul>
+        </Aside>
+        <Content>
+          <Route path="/icon" component={IconExample} />
+          <Route path="/dialog" component={DialogExample} />
+          <Route path="/layout" component={LayoutExample} />
+        </Content>
+      </Layout>
+      <Footer className="site-footer">&copy;&nbsp;Tuuu</Footer>
+    </Layout>
   </Router>,
   document.getElementById('root')
 );
